85ea69ee58f356ada37e93e476fe55177b15fc67
yukari.haneda0421 <yukari.haneda0421@gmail.com>
2023-02-07 19:25:57 +0900

    p194まで作成

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    public GameObject candyPrefab;
+    public GameObject[] candyPrefabs; //キャンディプロパティの配列化
+    public Transform candyParentTransform;
     public float shotForce;
     public float shotTorque;
+    public float baseWidth;
     
     // Start is called before the first frame update
     void Start()
@@ -20,13 +22,30 @@ public class Shooter : MonoBehaviour
         if(Input.GetButtonDown("Fire1")) Shot();
     }
 
+    //キャンディプレハブからランダムに1つを選ぶ
+    GameObject SampleCandy() {
+        int index = Random.Range(0, candyPrefabs.Length);
+        return candyPrefabs[index];
+    }
+
+    //発射位置の計算
+    Vector3 GetInstantiatePosition() {
+        //画面のサイズとInputの割合からキャンディ生成のポジションを計算
+        float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
+        return transform.position + new Vector3(x, 0, 0);
+    }
+
     public void Shot() {
         //プレハブからCandyオブジェクトを生成
-        GameObject candy = Instantiate (
-            candyPrefab,
-            transform.position,
+        GameObject candy = (GameObject)Instantiate (
+            SampleCandy(),
+            GetInstantiatePosition(),
             Quaternion.identity
             );
+
+        //生成したCandyオブジェクトの親をcandyParentTransformに設定する
+        candy.transform.parent = candyParentTransform;
+
         //candyオブジェクトのRigidbodyを取得し力と回転を加える
         Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
         candyRigidBody.AddForce(transform.forward * shotForce);

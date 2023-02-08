d66d2c801057431b758d2f6ae58088962e9fd4a9
yukari.haneda0421 <yukari.haneda0421@gmail.com>
2023-02-08 13:48:31 +0900

    p207まで作成

--- Assets/Scripts/CandyManager.cs
+++ Assets/Scripts/CandyManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class CandyManager : MonoBehaviour
 {
     const int DefaultCandyAmount = 30;
+    const int RecoverySeconds = 10;
 
     //現在のキャンディのストック数
     public int candy = DefaultCandyAmount;
+    //ストック回復までの残りの秒数
+    int counter;
 
     public void ConsumeCandy() {
         if (candy > 0) candy--;
@@ -27,6 +30,9 @@ public class CandyManager : MonoBehaviour
         //キャンディのストック数を表示
         string label = "Candy :" + candy;
 
+        //回復カウントしている時だけ秒数を表示
+        if (counter > 0) label = label + "(" + counter + "s)";
+
         GUI.Label(new Rect(50, 50, 100, 30), label);
     }
 
@@ -41,6 +47,22 @@ public class CandyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //キャンディストックがデフォルトより少なく、
+        //回復カウントしていないときにカウントをスタートさせる
+        if (candy < DefaultCandyAmount && counter <= 0) {
+            StartCoroutine(RecoverCandy()); //コルーリンのスタート
+        }
+    }
+
+    IEnumerator RecoverCandy() {
+        counter = RecoverySeconds;
+
+        //1秒ずつカウントを進める
+        while(counter > 0) {
+            yield return new WaitForSeconds(1.0f);
+            counter--;
+        }
+
+        candy++;
     }
 }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     public GameObject[] candyPrefabs; //キャンディプロパティの配列化
-    public Transform candyParentTransform;
+    public Transform candyParentTransform; //Candiesオブジェクトの子要素に配置するため親要素を準備
     public CandyManager candyManager;
     public float shotForce;
     public float shotTorque;
-    public float baseWidth;
+    public float baseWidth; //台の幅
     
     // Start is called before the first frame update
     void Start()
@@ -25,15 +25,24 @@ public class Shooter : MonoBehaviour
 
     //キャンディプレハブからランダムに1つを選ぶ
     GameObject SampleCandy() {
-        int index = Random.Range(0, candyPrefabs.Length);
+        int index = Random.Range(0, candyPrefabs.Length); //Rangeは以上・未満
         return candyPrefabs[index];
     }
 
     //発射位置の計算
     Vector3 GetInstantiatePosition() {
         //画面のサイズとInputの割合からキャンディ生成のポジションを計算
-        float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
-        return transform.position + new Vector3(x, 0, 0);
+        float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2); //-2.5~2.5
+        //Input.mousePosition.x:クリックしたx座標
+        //Screen.width
+
+        // 画面に関する情報を試しに取得
+        // Debug.Log(Input.mousePosition.x + ":" + Input.mousePosition.y);
+        // Debug.Log(Screen.width);
+        // Debug.Break();
+        
+        //左端からのクリックや右端からのクリックなど場所によって発射位置を算出
+        return transform.position + new Vector3(x, 0, 0); //shooterのtransform.positionは真ん中
     }
 
     public void Shot() {

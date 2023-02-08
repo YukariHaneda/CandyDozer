74380b8be5931d7052041f083e990c3f61a62f0d
yukari.haneda0421 <yukari.haneda0421@gmail.com>
2023-02-08 11:07:01 +0900

    p202まで作成

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,6 +6,7 @@ public class Shooter : MonoBehaviour
 {
     public GameObject[] candyPrefabs; //キャンディプロパティの配列化
     public Transform candyParentTransform;
+    public CandyManager candyManager;
     public float shotForce;
     public float shotTorque;
     public float baseWidth;
@@ -36,6 +37,9 @@ public class Shooter : MonoBehaviour
     }
 
     public void Shot() {
+        //キャンディを生成できる条件外ならばShotしない
+        if (candyManager.GetCandyAmount() <= 0) return;
+
         //プレハブからCandyオブジェクトを生成
         GameObject candy = (GameObject)Instantiate (
             SampleCandy(),
@@ -50,5 +54,8 @@ public class Shooter : MonoBehaviour
         Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
         candyRigidBody.AddForce(transform.forward * shotForce);
         candyRigidBody.AddTorque(new Vector3(0, shotTorque, 0));
+
+        //Candyのストックを消費
+        candyManager.ConsumeCandy();
     }
 }
